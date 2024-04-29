ff48dd61add44f7fbb41351b3f684228726a74ae
Safeer <safeeralam997@gmail.com>
2024-04-29 20:03:00 +0530

    conditional rendering using logical operator

--- Topics/map-method/src/App.jsx
+++ Topics/map-method/src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  let healthyFoods = [
-    "Green Vegetables",
-    "Protein rich food",
-    "Fiber rich food",
-    "Sea food",
-  ];
+  // let healthyFoods = [
+  //   "Green Vegetables",
+  //   "Protein rich food",
+  //   "Fiber rich food",
+  //   "Sea food",
+  // ];
 
-  // let healthyFoods = [];
+  let healthyFoods = [];
 
   if(healthyFoods.length===0){
     return  <center><h3>No food items to display</h3></center>
@@ -19,7 +19,7 @@ function App() {
     <>
       <center>
         <h1>Healthy Foods</h1>
-        {healthyFoods.length===0? <center><h3>No food items to display</h3></center> : null }
+        {healthyFoods.length===0 &&  <center><h3>No food items to display</h3></center>}
         <ol className="list-group list-group-numbered">
           {healthyFoods.map((item, index) => (
             <li key={index} className="list-group-item">
