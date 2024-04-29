06549ee58d6f7cf0d053255e34aa1f3aa33f5d80
Safeer <safeeralam997@gmail.com>
2024-04-29 19:48:53 +0530

    if-else statements

--- Topics/map-method/src/App.jsx
+++ Topics/map-method/src/App.jsx
@@ -6,14 +6,25 @@ function App() {
     "Green Vegetables",
     "Protein rich food",
     "Fiber rich food",
-    "Sea food"
+    "Sea food",
   ];
+
+  // let healthyFoods = [];
+
+  if(healthyFoods.length===0){
+    return  <center><h3>No food items to display</h3></center>
+  }
+
   return (
     <>
       <center>
         <h1>Healthy Foods</h1>
         <ol className="list-group list-group-numbered">
-          { healthyFoods.map((item,index)=><li key={index} className="list-group-item">{item}</li>)}
+          {healthyFoods.map((item, index) => (
+            <li key={index} className="list-group-item">
+              {item}
+            </li>
+          ))}
         </ol>
       </center>
     </>
