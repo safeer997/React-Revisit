5c46e36318f027e5dae19cc1e765e85b4f2247bf
Safeer <safeeralam997@gmail.com>
2024-05-18 01:13:33 +0530

    bharat clock - v2

--- Projects/Bharat-clock/src/components/CurrentTIme.jsx
+++ Projects/Bharat-clock/src/components/CurrentTIme.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+
 let CurrentTime = () => {
-  let time = new Date();
-  return <p className="lead">This is the current time : {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>;
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      // console.log("cleanup function called !") this was getting printed due to strict mode in main component .
+      clearInterval(timer);
+    }; //we must always pass a cleanup function instead of directly returning !!!
+  }, []);
+
+  return (
+    <p className="lead">
+      This is the current time : {time.toLocaleDateString()} -{" "}
+      {time.toLocaleTimeString()}
+    </p>
+  );
 };
 export default CurrentTime;
